979b1e5ee296c85ad5a62ac4b1ba3a1b733224c3
AThevon <adrienthevon@gmail.com>
2026-01-24 22:23:25 +0100

    chore: bump version to 1.6.0

--- wt.sh
+++ wt.sh
@@ -7,7 +7,7 @@
 # Tous les messages vont sur stderr pour ne pas polluer le résultat
 # =============================================================================
 
-VERSION="1.5.0"
+VERSION="1.6.0"
 
 # =============================================================================
 # Options de ligne de commande
