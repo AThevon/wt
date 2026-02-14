2973b195bb89ed5fdd969e2c0045d5217e5965f9
AThevon <adrienthevon@gmail.com>
2026-02-14 12:41:25 +0100

    chore: bump version to 1.7.2

--- wt.sh
+++ wt.sh
@@ -7,7 +7,7 @@
 # Tous les messages vont sur stderr pour ne pas polluer le résultat
 # =============================================================================
 
-VERSION="1.7.1"
+VERSION="1.7.2"
 
 # =============================================================================
 # Options de ligne de commande
