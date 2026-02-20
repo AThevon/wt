4eb1f6c24e381d29fa8cfa6b386593f3052d3249
AThevon <adrienthevon@gmail.com>
2026-02-20 16:20:25 +0100

    chore: bump version to 1.9.1

--- wt.sh
+++ wt.sh
@@ -7,7 +7,7 @@
 # Tous les messages vont sur stderr pour ne pas polluer le résultat
 # =============================================================================
 
-VERSION="1.9.0"
+VERSION="1.9.1"
 
 # =============================================================================
 # Options de ligne de commande
