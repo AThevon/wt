9ec9760b338c07499926f207f6af7b2b92a06087
AThevon <adrienthevon@gmail.com>
2026-03-26 17:38:54 +0100

    bump: v1.10.1

--- wt.sh
+++ wt.sh
@@ -7,7 +7,7 @@
 # Tous les messages vont sur stderr pour ne pas polluer le résultat
 # =============================================================================
 
-VERSION="1.10.0"
+VERSION="1.10.1"
 
 # =============================================================================
 # Options de ligne de commande
