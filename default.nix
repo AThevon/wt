444f5fa6c612cf4cb14cb1cef3bef434d3de4012
AThevon <adrienthevon@gmail.com>
2026-03-28 18:04:26 +0100

    update install.sh + default.nix: add gum dep, install lib/

--- default.nix
+++ default.nix
@@ -1,24 +1,23 @@
-{ lib, stdenvNoCC, makeWrapper, fzf, gh, jq, glab }:
+{ lib, stdenvNoCC, makeWrapper, fzf, gum, gh, jq, glab }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "wt";
-  version = "1.10.0";
+  version = "2.0.0";
 
   src = ./.;
 
   nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
-    # Main script
     install -Dm755 wt.sh $out/bin/wt-core
-
-    # Zsh completion
+    mkdir -p $out/lib
+    cp lib/*.sh $out/lib/
     install -Dm644 completions/wt.zsh $out/share/zsh/site-functions/_wt
   '';
 
   postFixup = ''
     wrapProgram $out/bin/wt-core \
-      --prefix PATH : ${lib.makeBinPath [ fzf gh jq glab ]}
+      --prefix PATH : ${lib.makeBinPath [ fzf gum gh jq glab ]}
   '';
 
   meta = with lib; {
